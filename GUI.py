4bff25a1074ba97dff9d42b2bc4452f06890502b
VitorLichtenberger <vitor.lichtenberger@student.uni-tuebingen.de>
2024-11-14 11:28:57 +0100

    Changed Gui to show selection window

--- GUI.py
+++ GUI.py
@@ -1,13 +1,16 @@
 from merge import *
 
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog, messagebox
+from tkinter.filedialog import askopenfilename
+
+import glob
+import os
 
 # Define the function to call when ButtonOk is clicked...
-def btnOk1_clicked():
-    x = int(entry1.get())
-    y = int(entry2.get())
-    mergePDF(x,y)
+def btnselect_clicked():
+    pdf_list = select_all_pdfs()
+    mergePDF(pdf_list)
     
 def btnOk2_clicked():
     x = entry3.get()
@@ -18,6 +21,30 @@ def btnOk2_clicked():
 def btnCancel_clicked():
     window.destroy()  
 
+def open_file_dialog(ent):
+    # Open file dialog and get the filename
+    filename = askopenfilename()
+    # Set the filename in entry4
+    ent.delete(0, 'end')
+    ent.insert(0, filename)
+    
+# Function to open a directory dialog and get all PDF files
+def select_all_pdfs():
+    # Open a dialog to select a folder
+    folder_path = filedialog.askdirectory()
+    list_of_PDF = []
+    # Check if a folder was selected
+    if folder_path:
+        # Use glob to find all PDFs in the selected folder
+        pdf_files = glob.glob(os.path.join(folder_path, "*.pdf"))
+        
+        # Display or process the PDF files
+        if pdf_files:
+            return pdf_files
+            
+        else:
+            print("No PDF files found in the selected directory.")
+    
 # Create the Window and give it a title...
 window = Tk()
 window.title("PDFJOINER")
@@ -33,47 +60,53 @@ window.tk.call('tk', 'scaling', 3.0)
 #############
 
 # Create our label...
-lbl = Label(window, text="Here you can Concatenate two PDFs by Number")
+lbl = Label(window, text="Here you can Concatenate all PDFs in a Folder")
 lbl.grid(column=0, row=0)
 
+# # Create our label...
+# lbl = Label(window, text="Enter a start number:")
+# lbl.grid(column=0, row=1)
+
+# # Create the entry box
+# entry1 = Entry(window, textvariable="Start")
+# entry1.grid(column=1, row=1)
+
+# # Create our label...
+# lbl = Label(window, text="Enter a end number:")
+# lbl.grid(column=0, row=2)
+
+# # Create the entry box
+# entry2 = Entry(window, textvariable="End")
+# entry2.grid(column=1, row=2)
+
+
+
+# Button to open the directory dialog and list PDFs
+btn_select_pdfs = Button(window, text="Select PDFs", command=btnselect_clicked)
+btn_select_pdfs.grid(column=1, row=0)
+
+
 # Create our label...
-lbl = Label(window, text="Enter a Start number: ")
-lbl.grid(column=0, row=1)
-
-# Create the entry box
-entry1 = Entry(window, textvariable="Start")
-entry1.grid(column=1, row=1)
-
-# Create our label...
-lbl = Label(window, text="Enter a end number: ")
-lbl.grid(column=0, row=2)
-
-# Create the entry box
-entry2 = Entry(window, textvariable="End")
-entry2.grid(column=1, row=2)
-
-btnOk = Button(window, text="Ok", command=btnOk1_clicked)
-btnOk.grid(column=0, row=3)
-
-# Create our label...
-lbl = Label(window, text="Here you can Concatenate two PDFs by Name")
+lbl = Label(window, text="Here you can Concatenate two PDFs")
 lbl.grid(column=0, row=4)
 
 # Create our label...
-lbl = Label(window, text="Enter first Name: ")
+lbl = Label(window, text="Select first: ")
 lbl.grid(column=0, row=5)
 
-# Create the entry box
-entry3 = Entry(window, textvariable="First")
+entry3 = Entry(window)
 entry3.grid(column=1, row=5)
+# Bind the entry4 box to open the file dialog on click
+entry3.bind("<Button-1>", lambda e: open_file_dialog(entry3))
 
 # Create our label...
-lbl = Label(window, text="Enter second Name: ")
+lbl = Label(window, text="Select second: ")
 lbl.grid(column=0, row=6)
 
-# Create the entry box
-entry4 = Entry(window, textvariable="Second")
+entry4 = Entry(window)
 entry4.grid(column=1, row=6)
+# Bind the entry4 box to open the file dialog on click
+entry4.bind("<Button-1>", lambda e: open_file_dialog(entry4))
 
 btnOk2 = Button(window, text="Ok", command=btnOk2_clicked)
 btnOk2.grid(column=0, row=7)
